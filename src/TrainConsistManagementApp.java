457a77ccf44a76eea8a96c99be43faaae2021661
Amay-Git101 <amayjariwala@gmail.com>
2026-04-17 10:14:52 +0530

    UC2: Add passenger bogies using ArrayList operations

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -7,10 +7,29 @@ public class TrainConsistManagementApp {
 
     public static void main(String[] args) {
         System.out.println("=== Train Consist Management App ===");
-        displayBogieCount();
+
+        addPassengerBogies();
+        displayBogies();
+        removeBogie("AC Chair");
+        checkBogie("Sleeper");
+        displayBogies();
     }
 
-    public static void displayBogieCount() {
-        System.out.println("Initial Bogie Count: " + trainConsist.size());
+    public static void addPassengerBogies() {
+        trainConsist.add("Sleeper");
+        trainConsist.add("AC Chair");
+        trainConsist.add("First Class");
+    }
+
+    public static void displayBogies() {
+        System.out.println("Bogies: " + trainConsist);
+    }
+
+    public static void removeBogie(String bogie) {
+        trainConsist.remove(bogie);
+    }
+
+    public static void checkBogie(String bogie) {
+        System.out.println(bogie + " exists: " + trainConsist.contains(bogie));
     }
 }
